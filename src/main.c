079a5c4639d6ecb583884e6c0c41bf0885e172a6
hammerandnail <yelionel26@gmail.com>
2021-02-13 15:47:28 +0800

    66. Get Lua Global Values in C

--- src/main.c
+++ src/main.c
@@ -12,8 +12,21 @@ void lua_example_dofile(void)
     lua_close(L);
 }
 
+void lua_example_getvar(void)
+{
+    lua_State *L = luaL_newstate();
+    luaL_dostring(L, "some_var = 486"); // executing a Lua string
+    lua_getglobal(L, "some_var"); // getting back a global varible from Lua
+    lua_Number some_var_in_c = lua_tonumber(L, -1);
+
+    printf("The value of some_var inside C is: %d\n", (int)some_var_in_c);
+
+    lua_close(L);
+}
+
 int main(int args, char *argv[])
 {
-    lua_example_dofile();
+    // lua_example_dofile();
+    lua_example_getvar();
     return 0;
 }
